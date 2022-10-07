53da1d32d3ce0098315e5680b666344636c7899e
Mohamedadil072 <112887984+Mohamedadil072@users.noreply.github.com>
2022-10-07 04:17:12 -0700

    Add files via upload

---
+++ Oddoreven.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Oddoreven{
+  public static void main(String[] args){
+    int num;
+    System.out.println("Enter an integer number:");
+    Scanner input=new Scanner(System.in);
+    num=input.nextInt();
+    if(num%2==0)
+      System.out.println("Entered number is even");
+    else
+      System.out.println("Enterd number is odd");
+  }
+}
